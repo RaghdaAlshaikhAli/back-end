c4f899a5304b5023c4a7dde316e1128fc8feca3b
Reem <104098773+reemossamaahmed@users.noreply.github.com>
2023-07-04 00:56:54 +0300

    Add files via upload

---
+++ model/lesson.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
+const lessonSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    url :{
+        type:String,
+        required:true,
+        trim:true, 
+        validate: { 
+            validator: value => validator.isURL(value, { protocols: ['http','https','ftp'], require_tld: true, require_protocol: true }),
+            message: 'Must be a Valid URL' 
+        }
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
+});
+
+const LessonModel = mongoose.model('lessons' , lessonSchema)
+
+module.exports = LessonModel
